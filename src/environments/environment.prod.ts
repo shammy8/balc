af9f44b9b223c143b2938131eedea786633be335
Allan Sham <allansham@hotmail.com>
2022-07-06 15:14:47 +0100

    install angular fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'bill-calculator-eaac9',
+    appId: '1:417329005138:web:6ae288a575f943f20ad58b',
+    databaseURL: 'https://bill-calculator-eaac9.firebaseio.com',
+    storageBucket: 'bill-calculator-eaac9.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAKULRE-nJF2WpDGr-RFxYNmHQAnXx675I',
+    authDomain: 'bill-calculator-eaac9.firebaseapp.com',
+    messagingSenderId: '417329005138',
+    measurementId: 'G-DE0KR5LFN7',
+  },
   production: true
 };
