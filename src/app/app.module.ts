264507ff724a675ccf467efc2988d02ac9186f05
Allan Sham <allansham@hotmail.com>
2022-07-06 15:41:00 +0100

    add primeng theme, add import for primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ConfirmationService, SharedModule } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { DataViewModule } from 'primeng/dataview';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { TooltipModule } from 'primeng/tooltip';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { CalendarModule } from 'primeng/calendar';
+import { ChipsModule } from 'primeng/chips';
+import { MenuModule } from 'primeng/menu';
+import { SidebarModule } from 'primeng/sidebar';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { DividerModule } from 'primeng/divider';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ProgressBarModule } from 'primeng/progressbar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
 import { MainAppComponent } from './main-app/main-app.component';
 import { BillComponent } from './bill/bill.component';
 import { NoBillComponent } from './no-bill/no-bill.component';
@@ -19,7 +38,7 @@ import { LoginComponent } from './login/login.component';
     MainAppComponent,
     BillComponent,
     NoBillComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,9 +46,30 @@ import { LoginComponent } from './login/login.component';
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
-    provideFunctions(() => getFunctions())
+    provideFunctions(() => getFunctions()),
+    SharedModule,
+    ButtonModule,
+    DropdownModule,
+    DataViewModule,
+    MultiSelectModule,
+    InputSwitchModule,
+    TooltipModule,
+    DialogModule,
+    InputTextModule,
+    InputNumberModule,
+    CalendarModule,
+    ChipsModule,
+    MenuModule,
+    SidebarModule,
+    SplitButtonModule,
+    DividerModule,
+    ConfirmPopupModule,
+    ProgressBarModule,
+    // ClipboardModule,
+    // OrderModule,
+    // EditableModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ConfirmationService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
