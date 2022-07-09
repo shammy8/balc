c032c246f6f3de8e43720233e62a3be2ddabbd37
Allan Sham <allansham@hotmail.com>
2022-07-09 11:45:48 +0100

    update no bill component

--- src/app/no-bill/no-bill.component.ts
+++ src/app/no-bill/no-bill.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'balc-no-bill',
-  templateUrl: './no-bill.component.html',
-  styleUrls: ['./no-bill.component.scss']
+  template: `
+    <div class="header">
+      <span class="bill-name"> Select a bill </span>
+      <p-splitButton
+        label="Add item"
+        icon="pi pi-plus"
+        appendTo="body"
+        [disabled]="true"
+      ></p-splitButton>
+      <!-- [model]="menuItems"
+        (onClick)="openAddItemDialog()" -->
+    </div>
+    <div class="items">
+      <p style="font-size: 1.2rem">
+        Please select a bill or add a new bill from the side menu.
+      </p>
+    </div>
+  `,
+  styleUrls: ['../bill/bill.component.scss'], // shares a stylesheet with bill component
 })
-export class NoBillComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class NoBillComponent {}
