c57f770dc046ea479b66de0300babdaa96d42178
Allan Sham <allansham@hotmail.com>
2022-07-09 11:58:45 +0100

    ng add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { ClipboardModule } from '@angular/cdk/clipboard';
 
@@ -110,6 +111,12 @@ import { TotalSpendingsComponent } from './total-spendings/total-spendings.compo
     ProgressBarModule,
     OrderModule,
     EditableModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [ConfirmationService],
   bootstrap: [AppComponent],
